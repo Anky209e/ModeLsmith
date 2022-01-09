40f02c9d3ef59cf5d39d32555eaa2d744f366de0
Ashwani Ahlawat <49407895+AshuAhlawat@users.noreply.github.com>
2022-01-09 23:39:16 +0530

    Iris Model back-end structure made

---
+++ nn/classes/iris.py
@@ -0,0 +1,38 @@
+import torch
+import numpy as np
+
+class IrisModel(torch.nn.Module):
+        def __init__(self,input_dim, output_dim):
+            super().__init__()
+            self.linear = torch.nn.Linear(input_dim, output_dim) 
+        
+        def forward(self, inputs):
+            relud = torch.relu(self.linear(inputs))
+            targets_pred = torch.softmax(relud,dim=-1) 
+            return targets_pred
+
+def predict_iris(sepal_length, sepal_width, petal_length, petal_width):
+    inputs = [sepal_length, sepal_width, petal_length, petal_width]
+    mean = [5.843333333333334, 3.0540000000000003, 3.758666666666666, 1.1986666666666668]
+    std = [0.8253012917851409, 0.4321465800705435, 1.7585291834055212, 0.7606126185881716]
+    
+    for i in range(4):
+        inputs[i] = (inputs[i] - mean[i])/ std[i]
+    inputs = torch.from_numpy(np.array(inputs).astype(np.float32))
+
+    logistic_model = IrisModel(4,3)
+    logistic_model.load_state_dict(torch.load("./models/Iris.pth"))
+
+    pred = logistic_model(inputs)
+    
+    pred = pred.detach().numpy().reshape(-1)
+    ans = np.where(pred == max(pred))
+    
+    if ans[0][0]==0:
+        final_ans = "Iris-setosa"
+    elif ans[0][0]==1:
+        final_ans = "Iris-versicolor"
+    else:
+        final_ans = "Iris-virginica"
+    
+    return final_ans,round(float(pred.max())*100, 2)
--- nn/views.py
+++ nn/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+import torch
+import numpy
+
+
+def iris(request):
+    from .classes.iris import predict_iris
+
+    values = [5,3,2,1]
+    x = predict_iris(*(values))
+
+    return render(request, "base.html", {"pred" : x[0], "prob" : x[1]})
