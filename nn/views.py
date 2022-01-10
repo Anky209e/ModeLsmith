29f47a8ae8ba5c190f8a753dd53f75d3a558783e
Ashwani Ahlawat <49407895+AshuAhlawat@users.noreply.github.com>
2022-01-10 14:37:09 +0530

    Front-end to back-end data loop complete

--- nn/views.py
+++ nn/views.py
@@ -6,18 +6,45 @@ import torch
 import numpy
 
 
+#----------------------IRIS---------------------
 def iris(request):
+    return render(request, "iris.html")
+
+def iris_ans(request):
     from .classes.iris import predict_iris
 
-    values = [4.8,3.4,1.6,0.2]
-    result = predict_iris(*(values))
+    sl = float(request.GET.get("sl"))
+    sw = float(request.GET.get("sw"))
+    pl = float(request.GET.get("pl"))
+    pw = float(request.GET.get("pw"))
 
-    return render(request, "base.html", {"pred" : result[0], "prob" : result[1]})
+    result = predict_iris(sl,sw,pl,pw)
 
+    return render(request, "result.html", {"pred" : result[0], "prob" : result[1]})
+
+
+#----------------------HEART---------------------
 def heart(request):
+    return render(request, "heart.html")
+
+def heart_ans(request):
     from .classes.heart import predict_heart_attack
 
-    values = [50,1,0,144,200,0,0,126,1,0.9,1,0,3]
-    result = predict_heart_attack(*values)
+    age =float(request.GET.get("age"))
+    sex = float(request.GET.get("sex"))
+    cp = float(request.GET.get("cp"))
+    trestbps = float(request.GET.get("trestbps"))
+    chol = float(request.GET.get("chol"))
+    fbs = float(request.GET.get("fbs"))
+    restecg = float(request.GET.get("restecg"))
+    thalach = float(request.GET.get("thalach"))
+    exang = float(request.GET.get("exang"))
+    oldpeak = float(request.GET.get("oldpeak"))
+    slope = float(request.GET.get("slope"))
+    ca = float(request.GET.get("ca"))
+    thal = float(request.GET.get("thal"))
 
-    return render(request,"base.html",{"pred":result[0],"prob":result[1]})
+    result = predict_heart_attack(age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal)
+
+    return render(request,"result.html",{"pred":result[0],"prob":result[1]})
+
