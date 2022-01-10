afc715ebe313b10b41cb0f35f5b5f0484b70286e
Ashwani Ahlawat <49407895+AshuAhlawat@users.noreply.github.com>
2022-01-10 17:14:56 +0530

    Changed forms in to POST method

--- nn/urls.py
+++ nn/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('iris/', views.iris),
-    path('iris/ans/',views.iris_ans),
+    # path('iris/ans/',views.iris_ans),
 
     path('heart/', views.heart),
     path('heart/ans/', views.heart_ans),
--- nn/views.py
+++ nn/views.py
@@ -8,24 +8,27 @@ import numpy
 
 #----------------------IRIS---------------------
 def iris(request):
-    return render(request, "iris.html")
+    if request.method =="POST":
+        from .classes.iris import predict_iris
 
-def iris_ans(request):
-    from .classes.iris import predict_iris
+        try:
+            sl = float(request.POST.get("sl"))
+            sw = float(request.POST.get("sw"))
+            pl = float(request.POST.get("pl"))
+            pw = float(request.POST.get("pw"))
+        except:
+            return render(request, "iris.html", {"pred" : "Enter the", "prob" : "Values First"})
 
-    sl = float(request.GET.get("sl"))
-    sw = float(request.GET.get("sw"))
-    pl = float(request.GET.get("pl"))
-    pw = float(request.GET.get("pw"))
+        result = predict_iris(sl,sw,pl,pw)
 
-    result = predict_iris(sl,sw,pl,pw)
+        return render(request, "iris.html", {"pred" : result[0], "prob" : result[1]})
 
-    return render(request, "result.html", {"pred" : result[0], "prob" : result[1]})
-
+    else:
+        return render(request, "iris.html", {"pred" : "Enter the", "prob" : "Values First"})
 
 #----------------------HEART---------------------
 def heart(request):
-    return render(request, "heart.html")
+    return render(request, "heart.html", {"pred" : "Dunno", "prob" : "00.00"})
 
 def heart_ans(request):
     from .classes.heart import predict_heart_attack
