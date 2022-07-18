3e123819a522391301010eb91eaba9ff1fabe52c
Ankit Kumar <nasaankit01@gmail.com>
2022-07-19 00:06:01 +0530

    poem_generation {added}

---
+++ nlp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NlpConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nlp'
---
+++ nlp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("", views.home),
+    path("poem_generation", views.poem_generation),
+]
+
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
